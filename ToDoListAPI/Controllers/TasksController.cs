e475e7f4760587411e59e56ca2565da91a32d9ba
Pushparajan <rdm96412@gmail.com>
2024-12-11 10:03:06 +0530

    added task in controller part

--- ToDoListAPI/Controllers/TasksController.cs
+++ ToDoListAPI/Controllers/TasksController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Runtime.InteropServices;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoListAPI.Data.Entities;
 using ToDoListAPI.Infra.Commands;
 using ToDoListAPI.Infra.Queries;
 
@@ -12,6 +15,8 @@ namespace ToDoListAPI.Controllers
     public class TasksController : ControllerBase
     {
         private readonly CreateTaskHandler _createHandler;
+        private readonly UpdateHandler _updateHandler;
+        private readonly DeleteHandler _deleteHandler;
         private readonly GetTasksHandler _queryHandler;
 
         public TasksController(CreateTaskHandler createHandler, GetTasksHandler queryHandler)
@@ -34,11 +39,40 @@ namespace ToDoListAPI.Controllers
             var tasks = await _queryHandler.Handle(query);
             return Ok(tasks);
         }
-        //public async Task<IActionResult> DeleteTask(DeleteTaskCommand command)
-        //{
-        //    var taskId = await _deleteHandler.Handle(command);
 
-        //    return Ok(GetTasks);
-        //}
+        [HttpPut("{id}")]
+        public IActionResult UpdateTask(int id, TaskItem updatedTask)
+        {
+            _updateHandler.Handle(id, updatedTask);
+            //var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            //var task = _context.Tasks.FirstOrDefault(t => t.Id == id && t.UserId == userId);
+
+            //if (task == null)
+            //    return NotFound();
+
+            //task.Title = updatedTask.Title;
+            //task.Description = updatedTask.Description;
+            //task.Status = updatedTask.Status;
+
+            //_context.SaveChanges();
+
+            return Ok("Task updated successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            _deleteHandler.Handle(id);
+            //var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            //var task = _context.Tasks.FirstOrDefault(t => t.Id == id && t.UserId == userId);
+
+            //if (task == null)
+            //    return NotFound();
+
+            //_context.Tasks.Remove(task);
+            //_context.SaveChanges();
+
+            return Ok("Task deleted successfully.");
+        }
     }
 }
